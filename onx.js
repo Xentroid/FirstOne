3f35b7e23e19c2ec0ed42f7c55f7eba45c54bf61
Richard Poulter <richpoulter@hotmail.com>
2025-07-22 15:33:43 +1200

    Addition of code to calculate game state value

--- onx.js
+++ onx.js
@@ -3,6 +3,7 @@ const wohWon = document.querySelector('#whoWon');
 const resetButton = document.querySelector("#resetButton");
 const radioO = document.querySelector("#radioO");
 const radioX = document.querySelector("#radioX");
+const moveScores = document.querySelector("#moveScores");
 
 let moves = 0;
 let who = 'O';
@@ -13,7 +14,11 @@ let gameState = [
 ];
 let win = false;
 let start = 'O';
+let oScore = 0;
+let xScore = 0;
 
+
+// Function to reset everything after a game concludes
 function reset() {
     let cell = '';
     win = false;
@@ -31,10 +36,14 @@ function reset() {
         tcell.innerHTML = '';
     })
     resetButton.style.display = "none";
+    oScore = 0;
+    xScore = 0;
+    moveScores.innerHTML = `The current score is O=0, X=0`;
     radioO.removeAttribute("disabled");
     radioX.removeAttribute("disabled");
 }
 
+// Checks to see if a win has occurred
 function checkWinStatus() {
     let wincell = '';
     for (let x = 0; x < 3; x++) {
@@ -90,9 +99,104 @@ function checkWinStatus() {
         wohWon.innerHTML = 'Nobody won that one';
         resetButton.style.display = "block";
     }
-
 }
 
+// Uses the count of Os or Xs on a paticuar row, column or diagonal to increase the score
+function rowScore(oCount, xCount) {
+    if (oCount === 3) {
+        oScore += 10;
+    } else if ((oCount === 2) && (xCount === 0)) {
+        oScore += 1;
+    }
+    if (xCount == 3) {
+        xScore += 10;
+    } else if ((xCount === 2) && (oCount === 0)) {
+        xScore += 1;
+    }
+}
+
+// Takes a state of play and calculates how strong a position it is for each player
+function positionScore(moveArray) {
+    let oCount = 0;
+    let xCount = 0;
+    xScore = 0;
+    oScore = 0;
+
+    for (let x = 0; x < 3; x++) {
+        oCount = 0;
+        xCount = 0;
+        for (let y = 0; y < 3; y++) {
+            if (moveArray[x][y] === 'O') {
+                oCount++;
+            } else if (moveArray[x][y] === 'X') {
+                xCount++;
+            }
+        }
+        rowScore(oCount, xCount);
+    }
+
+    for (let y = 0; y < 3; y++) {
+        oCount = 0;
+        xCount = 0;
+        for (let x = 0; x < 3; x++) {
+            if (moveArray[x][y] === 'O') {
+                oCount++;
+            } else if (moveArray[x][y] === 'X') {
+                xCount++;
+            }
+        }
+        rowScore(oCount, xCount);
+    }
+
+    oCount = 0;
+    xCount = 0;
+    if (moveArray[0][0] === 'O') {
+        oCount++;
+    }
+    if (moveArray[1][1] === 'O') {
+        oCount++;
+    }
+    if (moveArray[2][2] === 'O') {
+        oCount++;
+    }
+    if (moveArray[0][0] === 'X') {
+        xCount++;
+    }
+    if (moveArray[1][1] === 'X') {
+        xCount++;
+    }
+    if (moveArray[2][2] === 'X') {
+        xCount++;
+    }
+    rowScore(oCount, xCount);
+
+    oCount = 0;
+    xCount = 0;
+    if (moveArray[2][0] === 'O') {
+        oCount++;
+    }
+    if (moveArray[1][1] === 'O') {
+        oCount++;
+    }
+    if (moveArray[0][2] === 'O') {
+        oCount++;
+    }
+    if (moveArray[2][0] === 'X') {
+        xCount++;
+    }
+    if (moveArray[1][1] === 'X') {
+        xCount++;
+    }
+    if (moveArray[0][2] === 'X') {
+        xCount++;
+    }
+    rowScore(oCount, xCount);
+
+    moveScores.innerHTML = `The current score is O=${oScore}, X=${xScore}`;
+}
+
+
+// Adds an eventlistener for each cell, which does the business for each cell
 tcells.forEach(tcell => {
     tcell.addEventListener('click', function (event) {
         const cellClicked = event.target.id;
@@ -109,6 +213,9 @@ tcells.forEach(tcell => {
             tcell.innerHTML = who;
             if (moves >= 5) {
                 checkWinStatus();
+            }
+            if (moves >= 3) {
+                positionScore(gameState);
             }
             if (who === 'O') {
                 who = 'X';
